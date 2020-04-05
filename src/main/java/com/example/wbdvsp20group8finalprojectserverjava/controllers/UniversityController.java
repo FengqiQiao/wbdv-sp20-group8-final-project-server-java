303ba9b81e9b8e792e59e9451556187cadb7fd4d
Gcc <gcc@Gccs-MBP.verizon.net>
2020-04-05 17:06:17 -0400

    fix

--- src/main/java/com/example/wbdvsp20group8finalprojectserverjava/controllers/UniversityController.java
+++ src/main/java/com/example/wbdvsp20group8finalprojectserverjava/controllers/UniversityController.java
@@ -1,7 +1,5 @@
 package com.example.wbdvsp20group8finalprojectserverjava.controllers;
 
-
-
 import com.example.wbdvsp20group8finalprojectserverjava.services.UniversityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
