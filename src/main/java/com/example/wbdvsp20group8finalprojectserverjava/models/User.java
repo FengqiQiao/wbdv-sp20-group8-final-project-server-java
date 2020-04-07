b917898f8803e47f418bbdd14de7a902845a7574
FengqiQiao <59735370+FengqiQiao@users.noreply.github.com>
2020-04-07 17:00:20 -0400

    Fengqi (#4)
    * add role to user model

--- src/main/java/com/example/wbdvsp20group8finalprojectserverjava/models/User.java
+++ src/main/java/com/example/wbdvsp20group8finalprojectserverjava/models/User.java
@@ -10,6 +10,7 @@ public class User {
     private String email;
     private String username;
     private String password;
+    private String role;
 
     public Integer getId() {
         return id;
@@ -44,6 +45,15 @@ public class User {
         this.password = password;
     }
 
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    //    public User(int id, String email, String username, String password) {
 //    public User(int id, String email, String username, String password) {
 //        this.id = id;
 //        this.email = email;
